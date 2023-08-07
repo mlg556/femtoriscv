326fad7b7a352fe807719cc5646970f7c6ae0625
Mirac Gulgonul <mlg556@gmail.com>
2023-08-07 23:23:13 +0300

    load/store indexing

--- cpu.v
+++ cpu.v
@@ -8,16 +8,16 @@ module cpu (
 );
     `include "sext.v"
 
-
     // opcodes
-    localparam OPC_REG = 7'b0110011;  // add, sub, xor ...
-    localparam OPC_IMM = 7'b0010011;  // addi, xori, slli ...
-    localparam OPC_JAL = 7'b1101111;  // jal
-    localparam OPC_JALR = 7'b1100111;  // jalr
-    localparam OPC_BRANCH = 7'b1100011;  // beq, bne ...
-    localparam OPC_LUI = 7'b0110111;  // lui
-    localparam OPC_AUIPC = 7'b0010111;  // auipc
-    localparam OPC_LOAD = 7'b0000011;  // lb, lh ...
+    localparam OPC_REG = 7'b0110011;  // add, sub, xor ... rd = rs1 op rs2
+    localparam OPC_IMM = 7'b0010011;  // addi, xori, slli ... rd = rs1 op imm
+    localparam OPC_JAL = 7'b1101111;  // jal: jump and link
+    localparam OPC_JALR = 7'b1100111;  // jalr: jump and link register
+    localparam OPC_BRANCH = 7'b1100011;  // beq, bne ... branch if equal/notequal
+    localparam OPC_LUI = 7'b0110111;  // lui: load upper immediate
+    localparam OPC_AUIPC = 7'b0010111;  // auipc: add upper immediate to pc
+    localparam OPC_LOAD = 7'b0000011;  // lb, lh ... load from memory
+    localparam OPC_STORE = 7'b0100011;  // sb, sh, sw... store in memory
     localparam OPC_SYS = 7'b1110011;  // ebreak ... special system instructions
 
 
@@ -35,10 +35,20 @@ module cpu (
     reg signed [31:0] rs2 = 0;
 
     integer i;
-    `include "fib.v"  // program file
+    `include "loadstore.v"  // program file
     initial begin
         PC = 0;
         LOADMEM;  // loads memory file from mem.v
+
+        // store some words to be loaded
+        // Note: index 100 (word address)
+        //     corresponds to 
+        // address 400 (byte address)
+        MEM[100] = {8'h4, 8'h3, 8'h2, 8'h1};
+        MEM[101] = {8'h8, 8'h7, 8'h6, 8'h5};
+        MEM[102] = {8'hc, 8'hb, 8'ha, 8'h9};
+        MEM[103] = {8'hff, 8'hf, 8'he, 8'hd};
+
         instr = MEM[0];
 
         // zero all registers
@@ -68,13 +78,9 @@ module cpu (
     wire [ 6:0] funct7 = instr[31:25];
 
     // immediate fields
-
     wire [31:0] I_imm = sext12(instr[31:20]);
-
     wire [31:0] S_imm = sext12({instr[31:25], instr[11:7]});
-
     wire [31:0] B_imm = sext12({instr[31], instr[7], instr[30:25], instr[11:8], 1'b0});
-
     wire [31:0] U_imm = {instr[31:12], {12{1'b0}}};
     wire [31:0] J_imm = sext20({instr[31], instr[19:12], instr[20], instr[30:21], 1'b0});
 
@@ -83,14 +89,12 @@ module cpu (
     //wire [31:0] B_imm = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
     //wire [31:0] U_imm = {instr[31], instr[30:12], {12{1'b0}}};  // left shifted (right zero padded?)
 
-
     // I_type shift amount: I_imm[4:0]
     wire [ 4:0] shamt = rs2_idx;
 
-    // Load/store instruction sign extensions etc
-
-    wire [31:0] loadstore_addr = rs1 + I_imm;
-
+    // store/load addresses
+    wire [31:0] store_addr = rs1 + I_imm;
+    wire [31:0] load_addr = rs1 + S_imm;
 
     always @(posedge clk) begin
         // RESET
@@ -195,33 +199,58 @@ module cpu (
             end
 
             OPC_LUI: begin
-                RA[rd_idx] = U_imm;
+                RA[rd_idx] = U_imm;  // lui
                 PC = PC + 4;  // increment PC
             end
 
             OPC_AUIPC: begin
-                RA[rd_idx] = PC + U_imm;
+                RA[rd_idx] = PC + U_imm;  // auipc
                 PC = PC + 4;  // increment PC
 
             end
 
             OPC_LOAD: begin
+                // source register
                 rs1 = RA[rs1_idx];
 
+                // we need to sign-extend the 8 and 16 bits.
+                // zero-extension is done automagically by verilog in assignment.
                 case (funct3)
-                    3'h0: RA[rd_idx] = MEM[rs1+I_imm][7:0];  // lb | load byte, 8 bits
-                    3'h1: RA[rd_idx] = MEM[rs1+I_imm][15:0];  // lh | load half, 16 bits
-                    3'h2: RA[rd_idx] = MEM[rs1+I_imm][31:0];  // lw | load word, 32 bits
-                    3'h4: RA[rd_idx] = MEM[rs1+I_imm][7:0];  // lb | load byte, 8 bits
-                    3'h5: RA[rd_idx] = MEM[rs1+I_imm][7:0];  // lb | load byte, 8 bits
+                    3'h0:
+                    RA[rd_idx] =
+                        sext8(MEM[load_addr[31:2]][7:0]);  // lb | load byte, 8 bits, signext
+                    3'h1:
+                    RA[rd_idx] =
+                        sext16(MEM[load_addr[31:2]][15:0]);  // lh | load half, 16 bits, signext
+                    3'h2: begin
+                        RA[rd_idx] = MEM[load_addr[31:2]];  // lw | load word, 32 bits
+                        $display("RA[%d] = MEM[%d]", rd_idx, load_addr[31:2]);
+                    end
+                    3'h4:
+                    RA[rd_idx] = MEM[load_addr[31:2]][7:0];  // lbu | load byte unsigned, 8 bits, zeroext
+                    3'h5:
+                    RA[rd_idx] = MEM[load_addr[31:2]][15:0];  // lhu | load half unsigned, 16 bits, zeroext
                 endcase
                 PC = PC + 4;  // increment PC
 
             end
 
+            OPC_STORE: begin
+                // source registers
+                rs1 = RA[rs1_idx];
+                rs2 = RA[rs2_idx];
+
+                case (funct3)
+                    3'h0: MEM[rs1+S_imm][7:0] = rs2[7:0];  // sb: store byte
+                    3'h1: MEM[rs1+S_imm][15:0] = rs2[15:0];  // sh: store half
+                    3'h2: MEM[rs1+S_imm] = rs2;  // sw: store word
+                endcase
+                PC = PC + 4;
+            end
+
             OPC_SYS: begin
                 // basically nop, do NOT increment PC and finish simulation
-                $finish;
+                //$finish;
             end
 
         endcase
---
+++ loadstore.v
@@ -0,0 +1,7 @@
+task LOADMEM;
+	begin
+		MEM[0] = 32'h00000413;
+		MEM[1] = 32'h19042503;
+		MEM[2] = 32'h00100073;
+	end
+endtask

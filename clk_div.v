672d70aefa808878c031d70b0e6eded78fa95c0e
Miraç Gülgönül <mlg556@gmail.com>
2023-08-16 00:54:00 +0300

    Revert "state machine working"

--- clk_div.v
+++ clk_div.v
@@ -1,10 +1,9 @@
-module clk_div #(
-    parameter N = 2
-) (
+module clkdiv (
     input  clk,
     output clk_out
 );
     // slows down the clock by 2^N
+    localparam N = 21;
     reg [N:0] counter = 0;
 
     always @(posedge clk) begin
--- loadstore.v
+++ loadstore.v
@@ -1,30 +1,15 @@
-// OPC_LOAD: begin
-//     // source register
-//     rs1 = RA[rs1_idx];
-
-//     // we need to sign-extend the 8 and 16 bits.
-//     // zero-extension is done automagically by verilog in assignment.
-//     case (funct3)
-//         3'h0: RA[rd_idx] <= sext8(MEM[load_addr[31:2]][7:0]);  // lb
-//         3'h1: RA[rd_idx] <= sext16(MEM[load_addr[31:2]][15:0]);  // lh
-//         3'h2: RA[rd_idx] <= MEM[load_addr[31:2]];  // lw | load word, 32 bits
-//         3'h4: RA[rd_idx] <= MEM[load_addr[31:2]][7:0];  // lbu
-//         3'h5: RA[rd_idx] <= MEM[load_addr[31:2]][15:0];  // lhu
-//     endcase
-//     PC = PC + 4;  // increment PC
-
-// end
-
-// OPC_STORE: begin
-//     // source registers
-//     rs1 = RA[rs1_idx];
-//     rs2 = RA[rs2_idx];
-
-//     case (funct3)
-//         3'h0: MEM[store_addr[31:2]][7:0] <= rs2[7:0];  // sb: store byte
-//         3'h1: MEM[store_addr[31:2]][15:0] <= rs2[15:0];  // sh: store half
-//         3'h2: MEM[store_addr[31:2]] <= rs2;  // sw: store word
-//     endcase
-//     // $display("STORE: MEM[%0d] <= %0d", store_addr[31:2], rs2);
-//     PC = PC + 4;
-// end
+task LOADMEM;
+	begin
+		MEM[0] = 32'h00300293;
+		MEM[1] = 32'h00800113;
+		MEM[2] = 32'h18512823;
+		MEM[3] = 32'h00700293;
+		MEM[4] = 32'h00c00113;
+		MEM[5] = 32'h18512823;
+		MEM[6] = 32'h00800113;
+		MEM[7] = 32'h19012503;
+		MEM[8] = 32'h00410113;
+		MEM[9] = 32'h19012503;
+		MEM[10] = 32'h00100073;
+	end
+endtask
